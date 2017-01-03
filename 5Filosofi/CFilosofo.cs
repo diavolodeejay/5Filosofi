3c773925a0c3b314704a342e0abd199d71f4f179
Gabry_Laptop <gabriele.dinuovo>
2017-01-03 10:48:13 +0100

    Created a logic. I think

---
+++ 5Filosofi/CFilosofo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5Filosofi
+{
+    class CFilosofo
+    {
+        private int mNumero;
+
+        public int Numero
+        {
+            get
+            {
+                return mNumero;
+            }
+            set
+            {
+                if (value < 0)
+                    mNumero = 0;
+                else
+                    mNumero = value;
+            }
+        }
+
+    }
+}
